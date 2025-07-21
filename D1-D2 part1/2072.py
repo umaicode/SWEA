f94466bc8659acb88435ff46b2111fef420f9f77
umaicode <park.seungchan99@gmail.com>
2025-07-21 09:18:34 +0900

    2072: 홀수만 더하기

---
+++ D1-D2 part1/2072.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    result = 0
+    ls = input().split()
+    numbers = list(map(int, ls))
+    for number in numbers:
+        if number % 2 == 0:
+            pass
+        else:
+            result += number
+
+    print(f"#{test_case} {result}")
